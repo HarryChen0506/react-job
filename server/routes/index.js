c044f3b19dc9e523cb2a88726e7fcca008470b8c
Harry <chenwenhua0506@sina.com>
2017-11-27 23:01:31 +0800

    安装nodemon实时更新后端

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+//测试nodemon是否能实时更新
+router.get('/demo', function(req, res, next) {
+  res.json({code:200, name:"测试"})
+});
 module.exports = router;
