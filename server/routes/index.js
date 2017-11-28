df9b97d9fbb632835248c3e3ef2a79ffed5fd446
chenwenhua@juqitech.com <chenwenhua@juqitech.com>
2017-11-28 15:29:33 +0800

    引入antd-mobile

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,21 @@
 var express = require('express');
 var router = express.Router();
+//mongoose
+var mongoose = require('mongoose');
+//链接数据库
+mongoose.connect('mongodb://user:!QAZ2wsx@127.0.0.1:27017/demo',{useMongoClient:true});
+//监听是否链接成功
+mongoose.connection.on('connected', function (){
+    console.log('MongoDB connected success.')
+})
+mongoose.connection.on('error', function (){
+    console.log('MongoDB connected fail.')
+})
+mongoose.connection.on('disconnected', function (){
+    console.log('MongoDB connected disconnected.')
+})
+var Users = require('../models/demo.js');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,6 +24,23 @@ router.get('/', function(req, res, next) {
 
 //测试nodemon是否能实时更新
 router.get('/demo', function(req, res, next) {
-  res.json({code:200, name:"测试"})
+  // res.json({code:200, name:"测试"})
+  Users.find({}, function(err, doc){
+    res.json(doc)
+  })
 });
+router.get('/delete', function(req, res, next){
+  Users.remove({}, function(err, doc){
+    res.json(doc)
+  })
+})
+router.get('/create', function(req, res, next){
+  Users.create({
+    "name": "harry",
+    "age": 28,
+    "num": Math.floor(Math.random()*10)
+  },function(err, doc){
+    res.json(doc)
+  })
+})
 module.exports = router;
