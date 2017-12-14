3788f1c627b313222622cb5c01fdeeece0c4104f
chenwenhua@juqitech.com <chenwenhua@juqitech.com>
2017-12-14 17:34:48 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+
 //注入store
 import { Provider} from 'react-redux';
 import store from 'redux_module/store.js'
@@ -15,5 +16,6 @@ ReactDOM.render(
     </Provider> 
     ), 
     document.getElementById('root')
-);    
+);  
+   
 registerServiceWorker();
